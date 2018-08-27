21b36a9589779285fda0aab917101f1cba5f6c9b
Secure-Active-CIC <info@secure.coop>
2018-08-27 14:28:07 +0100

    Post installation script for Mint builds.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 
 #Bomb out if not sudo'd or run as root
 if [[ $EUID -ne 0 ]]; then
-   	echo "You'll need to run this as root"
+   	echo "$(tput setaf 2)You'll need to run this as root"
    	exit 1
 else
 
@@ -19,7 +19,7 @@ else
   echo ''
 
   #Update repos and upgrade without prompting
-  echo "Updating and Upgrading"
+  echo "$(tput setaf 2)Updating and Upgrading"
   apt-get update && sudo apt-get upgrade -y
 
   #Install some bits and bobs
@@ -31,14 +31,14 @@ else
   ufw default allow outgoing
   ufw enable
   #Ask if SSH needs to be enabled
-  echo -n "Do you need to use SSH on this box (y/n)? "
+  echo -n "$(tput setaf 2)Do you need to use SSH on this box (y/n)? "
   read -r answer
   if [ "$answer" != "${answer#[Yy]}" ] ;then
     ufw deny ssh
-    echo "SSH has been disallowed"
+    echo "$(tput setaf 2)SSH has been disallowed"
   else
     ufw allow ssh
-    echo "SSH has been allowed"
+    echo "$(tput setaf 2)SSH has been allowed"
   fi
 
   #Set root password - HASHED OUT FOR TESTING
@@ -52,11 +52,11 @@ else
   systemctl restart apparmor
 
   #Install KeepassXC
-  echo "Installing KeepassXC"
+  echo "$(tput setaf 2)Installing KeepassXC"
   apt-get install keepassxc
 
   #Install Wire
-  echo "Installaing Wire"
+  echo "$(tput setaf 2)Installaing Wire"
   wget -q https://wire-app.wire.com/linux/releases.key -O- | sudo apt-key add -
   echo "deb https://wire-app.wire.com/linux/debian stable main" | sudo tee /etc/apt/sources.list.d/wire-desktop.list
   apt-get update
@@ -68,13 +68,13 @@ else
   sudo apt update && sudo apt install signal-desktop
 
   #Install Veracrypt
-  echo "Installing Veracrypt..."
+  echo "$(tput setaf 2)Installing Veracrypt..."
   add-apt-repository ppa:unit193/encryption -y
 	apt-get update
 	apt-get install veracrypt -y
 
   #Install Tresorit
-  echo "Installing Tresorit..."
+  echo "$(tput setaf 2)Installing Tresorit..."
   cd ~
   mkdir Tresorit
   cd Tresorit
@@ -82,50 +82,50 @@ else
   sh ./tresorit_installer.run
 
   #Install Tor Browser
-  echo "Installing Tor Browser..."
+  echo "$(tput setaf 2)Installing Tor Browser..."
   apt-get install torbrowser-launcher
 
   #Install ProtonVPN
-  echo -n "Do you want to install ProtonVPN (y/n)? "
+  echo -n "$(tput setaf 2)Do you want to install ProtonVPN (y/n)? "
   read -r answer
   if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo "Installing ProtonVPN..."
+    echo "$(tput setaf 2)Installing ProtonVPN..."
     apt-get install -y openvpn python dialog sysctl sha512sum git
     git clone "https://github.com/protonvpn/protonvpn-cli"
     cd protonvpn-cli
     ./protonvpn-cli.sh --install
   else
-    echo "No ProtonVPN for you..."
+    echo "$(tput setaf 2)No ProtonVPN for you..."
   fi
 
   #Install the anti-virus ClamTK
-  echo "Installing ClamTK..."
+  echo "$(tput setaf 2)Installing ClamTK..."
   apt-get install -y clamtk
   mkdir ~/virus
-  echo "Scanning for malware, probably best to get a coffee..."
+  echo "$(tput setaf 2)Scanning for malware, probably best to get a coffee..."
   clamscan -r -i --move=$HOME/virus .
   systemctl stop clamav-freshclam
   sudo freshclam
   sudo systemctl start clamav-freshclam
 
   #Install RKHunter - a rootkit hunter
-  echo "Installing RKHunter..."
+  echo "$(tput setaf 2)Installing RKHunter..."
   apt-get install -y rkhunter && rkhunter --update && rkhunter --propupd
   #Add rkhunter to cron to run weekly
 
   #Remove Mono & Orca
-  echo "Removing Mono & Orca..."
+  echo "$(tput setaf 2)Removing Mono & Orca..."
   apt-get remove -y mono-runtime-common gnome-orca -y
 
   #Install Lynis - Security auditor
-  echo "Installing Lynis - Security Auditor..."
+  echo "$(tput setaf 2)Installing Lynis - Security Auditor..."
   sudo wget -O - https://packages.cisofy.com/keys/cisofy-software-public.key | sudo apt-key add -
   apt install apt-transport-https
   echo 'Acquire::Languages "none";' | sudo tee /etc/apt/apt.conf.d/99disable-translations
   echo "deb https://packages.cisofy.com/community/lynis/deb/ stable main" | sudo tee /etc/apt/sources.list.d/cisofy-lynis.list
   apt update
   apt-get install lynis
-  echo "To run Lynis enter 'lynis audit system'"
+  echo "$(tput setaf 2)To run Lynis enter 'lynis audit system'"
 
   #Sign off
   echo ''
