68de3aecd23e6f56fb27530f7a6e48989201b64e
Secure-Active-CIC <info@secure.coop>
2018-08-27 14:08:03 +0100

    Post Mint installation script.

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ else
 
   #Install Signal
   curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-  echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+  echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal-xenial.list
   sudo apt update && sudo apt install signal-desktop
 
   #Install Veracrypt
